3f18ee15b040dd867a587a3238eebbee084d5ca0
RenukaSrivalli <renuka1257@sasi.ac.in>
2022-07-19 21:57:16 +0530

    objectcreation

---
+++ CreationOfObjects.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Code
+{
+int j=0;
+public string names(String name)
+{
+System.out.println(j +"."name);
+j+=1;
+}
+}
+class CreationOfObjects
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter strength of class :");
+int s=sc.nextInt() ;
+
+for(int i=i;i<=s;i++)
+{
+   System.out.println("rollno :"+i);
+String name=sc.nextLine() ;                        
+Code obj=new Code();
+obj.names(name)
+}
